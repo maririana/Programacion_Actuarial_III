13bda85206569a3ea9a322fe0edc31809d15d643
maririana <ms.marianagarfias@gmail.com>
2016-03-03 22:47:05 -0600

    hola
    saludos

---
+++ holiif.R
@@ -0,0 +1,11 @@
+y <- runif(1)
+if(x>0.1 && x<0.2) {
+    y <- "¿Qué pedo wey?"
+} else if (x>0.2 && x<0.30) {
+    y <- "¿Qué onda?, Quiubo, ¿Qué pex?"
+} else if (x>0.3 && x<0.4) {
+    y <- "Hola"
+}
+ y    
+     
+  
